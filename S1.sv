bc39752b32db15a90455d41243b07ac63c0fc0cd
Tyson Fosdick <tfosdick@pdx.edu>
2018-10-18 22:55:45 -0700

    ModelSim local file

---
+++ S1.sv
@@ -0,0 +1,20 @@
+module patternMealy(input logic clk,input logic reset,input logic a,output logic y);
+typedef enum logic{S0, S1, S2, S3} statetype;
+statetype state, nextstate;
+
+// state register
+always_ff@(posedgeclk, posedgereset)
+	if (reset) state <= S0;
+else state <= nextstate;
+// next state logic
+always_comb
+case (state)
+S0: if (a) nextstate= S0;
+else nextstate= S1;
+S1: if (a) nextstate= S0;
+else nextstate= S1;
+default: nextstate= S0;
+endcase
+// output logic
+assign y = (a& state = = S1)
+endmodule
